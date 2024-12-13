b5d9a4a052521940abba14a4c4415985c71b467c
Harsh Kasundra <kasundraharsh0611@gmail.com>
2024-12-13 17:38:12 +0530

    Completed

--- frontend/src/services/axiosInstance.ts
+++ frontend/src/services/axiosInstance.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 export const axiosInstance = axios.create({
   baseURL: backendUrl,
-  timeout: 10000,
+  timeout: 60000,
   headers: { "content-type": "application/json" },
 });
